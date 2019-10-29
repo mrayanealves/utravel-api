3db305ae896a4523beb9291b9239585f5e89cb1a
Maria Rayane Alves <mrayalves05@gmail.com>
2019-10-29 10:52:33 -0300

    crud Veiculo Alugado

---
+++ src/br/ufrn/imd/utravel/controller/VeiculoAlugadoController.java
@@ -0,0 +1,21 @@
+package br.ufrn.imd.utravel.controller;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.ws.rs.Path;
+
+import br.ufrn.imd.utravel.model.VeiculoAlugado;
+import br.ufrn.imd.utravel.service.AbstractService;
+import br.ufrn.imd.utravel.service.VeiculoAlugadoService;
+
+@Stateless
+@Path("/veiculo-alugado")
+public class VeiculoAlugadoController extends AbstractController<VeiculoAlugado>{
+	@EJB
+	private VeiculoAlugadoService service;
+
+	@Override
+	protected AbstractService<VeiculoAlugado> service() {
+		return this.service;
+	}
+}
