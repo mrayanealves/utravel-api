1f78ac7fdfb940c6594d2ce71bf67a57cddfdc9e
Carlos Eduardo <carloseduardofox8@gmail.com>
2019-11-15 09:35:13 -0300

    Adiciona CORS #40

---
+++ src/br/ufrn/imd/utravel/config/CorsFilter.java
@@ -0,0 +1,19 @@
+package br.ufrn.imd.utravel.config;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+
+@Provider
+public class CorsFilter implements ContainerResponseFilter {
+    @Override
+    public void filter(ContainerRequestContext containerRequestContext, ContainerResponseContext containerResponseContext) throws IOException {
+        containerResponseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+        containerResponseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+        containerResponseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
+        containerResponseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        containerResponseContext.getHeaders().add("Access-Control-Max-Age", "1909600");
+    }
+}
