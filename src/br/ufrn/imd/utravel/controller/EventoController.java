692b6efa2662b6ba81665e7cb2483df96d685235
Rayane <mrayalves05@gmail.com>
2019-11-26 10:20:20 -0300

    Add mudanca em evento controller

--- src/br/ufrn/imd/utravel/controller/EventoController.java
+++ src/br/ufrn/imd/utravel/controller/EventoController.java
@@ -1,41 +1,43 @@
 package br.ufrn.imd.utravel.controller;
 
-import br.ufrn.imd.utravel.dto.EventoDTO;
-import br.ufrn.imd.utravel.mapper.EventoMapper;
-import br.ufrn.imd.utravel.model.Evento;
 import br.ufrn.imd.utravel.security.Secured;
-import br.ufrn.imd.utravel.service.AbstractService;
 import br.ufrn.imd.utravel.service.EventoService;
 import io.swagger.annotations.Api;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
-import javax.enterprise.context.RequestScoped;
-import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
-import java.util.List;
 
 @Api("Evento")
 @Stateless
 @Path("/evento")
-public class EventoController extends AbstractController<Evento> {
+public class EventoController {
 	@EJB 
 	private EventoService service;
-	@Inject
-	private EventoMapper eventoMapper;
-
-    @Override
-    protected AbstractService<Evento> service() {
-        return service;
+	
+	@GET
+    @Produces("application/json; charset=UTF-8")
+    @Path("/")
+    @Secured
+    public Response buscarTodos() {
+        return Response.ok(service.buscarTodos()).build();
     }
 
     @GET
-    @Path("/buscarPorViagemId/{viagemId}")
+    @Produces("application/json; charset=UTF-8")
+    @Path("/{id}")
+    @Secured
+    public Response buscarPorId(@PathParam("id") long id) {
+        return Response.ok(service.buscarPorId(id)).build();
+    }
+
+    @GET
+    @Path("/{id}/buscar")
     @Consumes("application/json; charset=UTF-8")
     @Produces("application/json; charset=UTF-8")
     @Secured
-    public Response buscarPorViagemId(@PathParam("viagemId") long viagemId) {
-        return Response.ok(service.buscarPorViagemId(viagemId)).build();
+    public Response buscarPorViagemId(@PathParam("id") long idViagem) {
+        return Response.ok(service.buscarPorViagemId(idViagem)).build();
     }
 }
